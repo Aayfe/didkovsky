0b8b1a218ef10cd5af61b2bfc9a2ae2a181056a7
Aayfe <135758282+Aayfe@users.noreply.github.com>
2025-04-04 18:51:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         downloadBtn.addEventListener("click", function () {
             window.location.href = "modpack.zip";
         });
+
+        // Stahování modpacku
+    const downloadBtn = document.getElementById("download-btn-java");
+    if (downloadBtn) {
+        downloadBtn.addEventListener("click", function () {
+            window.location.href = "https://download.oracle.com/java/17/archive/jdk-17.0.12_windows-x64_bin.exe";
+        });
     }
 });
